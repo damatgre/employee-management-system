bdcc8efe7db3c1c6fe1bf2f3ba751e37d6f1c3a0
damatgre <gregdamato2@gmail.com>
2021-07-17 08:59:55 -0400

    update connection

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   // Your MySQL username,
   user: 'root',
   // Your MySQL password
-  password: 'B3ar5B33t5Battl3',
+  password: '',
   database: 'employees_db'
 });
 
